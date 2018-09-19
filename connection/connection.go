4580a534e5051530830e4e125e7c0fee87dd2d3c
nrtkbb <nrtkbb@gmail.com>
2018-09-19 18:42:21 +0900

    ConnectAttrを蓄積し、後からヒストリーを検索できるパッケージの追加。

---
+++ connection/connection.go
@@ -0,0 +1,139 @@
+package connection
+
+import (
+	"github.com/nrtkbb/go-mayaascii"
+	"log"
+	"fmt"
+)
+
+type Connections struct {
+	SrcNodes conNodes
+	DstNodes conNodes
+}
+
+func (c *Connections) Append(con *mayaascii.ConnectAttr) {
+	c.SrcNodes.Append(con.SrcNode, con)
+	c.DstNodes.Append(con.DstNode, con)
+}
+
+func (c *Connections) Get(
+	node, attr string,
+	up bool,
+	inMap *map[string]*mayaascii.CreateNode) []*mayaascii.CreateNode {
+	sets := map[string]*mayaascii.CreateNode{}
+	if up {
+		cons := c.DstNodes.Get(node)
+		for _, con := range cons {
+			if con.DstAttr != attr {
+				continue
+			}
+			node, ok := (*inMap)[con.SrcNode]
+			if ok {
+				sets[node.NodeName] = node
+			}
+		}
+	} else {
+		cons := c.SrcNodes.Get(node)
+		for _, con := range cons {
+			log.Println(node, attr, con.SrcAttr)
+			if con.SrcAttr != attr {
+				continue
+			}
+			node, ok := (*inMap)[con.DstNode]
+			if ok {
+				sets[node.NodeName] = node
+			}
+		}
+	}
+	var results []*mayaascii.CreateNode
+	for _, v := range sets {
+		results = append(results, v)
+	}
+	return results
+}
+
+func (c *Connections) Search(
+	key string,
+	up bool,
+	inMap *map[string]*mayaascii.CreateNode) []*mayaascii.CreateNode {
+	histories := c.search(key, up)
+	sets := map[string]*mayaascii.CreateNode{}
+	if up {
+		for _, history := range histories {
+			value, ok := (*inMap)[history.DstNode]
+			if ok {
+				sets[value.NodeName] = value
+			}
+		}
+	} else {
+		for _, history := range histories {
+			value, ok := (*inMap)[history.SrcNode]
+			if ok {
+				sets[value.NodeName] = value
+			}
+		}
+	}
+	results := make([]*mayaascii.CreateNode, len(sets))
+	i := 0
+	for _, v := range sets {
+		results[i] = v
+		i++
+	}
+	return results
+}
+
+func (c *Connections) search(key string, up bool) []*mayaascii.ConnectAttr {
+	var histories []*mayaascii.ConnectAttr
+	if up {
+		values := c.DstNodes.Get(key)
+		histories = append(histories, values...)
+		for _, value := range values {
+			histories = append(histories, c.search(value.SrcNode, up)...)
+		}
+	} else {
+		values := c.SrcNodes.Get(key)
+		histories = append(histories, values...)
+		for _, value := range values {
+			histories = append(histories, c.search(value.DstNode, up)...)
+		}
+	}
+	return histories
+}
+
+type conNodes struct {
+	m map[string][]*mayaascii.ConnectAttr
+}
+
+func (c *conNodes) Length() int {
+	return len(c.m)
+}
+
+func (c *conNodes) String() string {
+	var buf []byte
+	buf = append(buf, "[\n"...)
+	for k, v := range c.m {
+		buf = append(buf, k...)
+		buf = append(buf, ": "...)
+		buf = append(buf, fmt.Sprintf("%v", v)...)
+		buf = append(buf, ",\n"...)
+	}
+	buf = append(buf, "]\n"...)
+	return string(buf)
+}
+
+func (c *conNodes) Get(key string) []*mayaascii.ConnectAttr {
+	if c.m == nil {
+		c.m = map[string][]*mayaascii.ConnectAttr{}
+	}
+	v, ok := c.m[key]
+	if ok {
+		return v
+	} else {
+		return []*mayaascii.ConnectAttr{}
+	}
+}
+
+func (c *conNodes) Append(key string, con *mayaascii.ConnectAttr) {
+	v := c.Get(key)
+	c.m[key] = append(v, con)
+}
